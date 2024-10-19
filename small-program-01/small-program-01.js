4bf6549edea1449269bc3ee8874b7f922a737b80
Anastasia Meicy <anastasiameicy@gmail.com>
2024-10-19 18:29:06 +0700

    Add comments and add code alternative to weather forecast file.

--- small-program-01/small-program-01.js
+++ small-program-01/small-program-01.js
@@ -9,21 +9,25 @@ Replace numbers divisible by both 3 and 5 with the text "FooBar."
 Print the numbers horizontally, not vertically.
 */
 
-// Array of numbers from 1 to 100, reversed + filtered non prime numbers only
+/* ARRAY OF NUMBERS FROM 1 TO 100 (REVERSED + FILTERED NON PRIME NUMBERS ONLY) */
 let arrNumbers = []
 for (let i = 100; i >= 1; i--) {
 	let isPrime = true
-	if (i <= 1) isPrime = false
+	if (i <= 1) {
+		isPrime = false
+	}
 	for (let j = 2; j < i; j++) {
 		if (i % j === 0) {
 			isPrime = false
 			break
 		}
 	}
-	if (!isPrime) arrNumbers.push(i)
+	if (!isPrime) {
+		arrNumbers.push(i)
+	}
 }
 
-// Foo, Bar, FooBar conditional
+/* FOO, BAR, FOOBAR CONDITIONAL */
 let output = []
 for (let i = 0; i < arrNumbers.length; i++) {
 	if (arrNumbers[i] % 3 === 0 && arrNumbers[i] % 5 === 0) {
--- weather-forecast-02/weather-forecast-02.js
+++ weather-forecast-02/weather-forecast-02.js
@@ -2,13 +2,13 @@ const axios = require('axios')
 
 require('dotenv').config()
 
-// Jakarta's latitude & longitude based on (https://www.latlong.net/place/jakarta-indonesia-27575.html)
+/* JAKARTA'S LATITUDE & LONGITUDE BASED ON : (https://www.latlong.net/place/jakarta-indonesia-27575.html) */
 let lat = -6.2
 let lon = 106.816666
 let API = process.env.OPENWEATHER_API
 let URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${API}`
 
-// Call getWeather API
+/* CALL GETWEATHER API */
 const getWeather = async () => {
 	try {
 		let weather = await axios.get(URL)
@@ -19,17 +19,38 @@ const getWeather = async () => {
 			day: 'numeric',
 		}
 
-		for (let i = 1; i < weather.data.list.length; i++) {
-			if (weather.data.list[i].dt_txt.endsWith('06:00:00')) {
-				let date = new Date(weather.data.list[i].dt_txt.split(' ')[0])
-				date = date.toLocaleDateString('id-ID', options)
-				let temperature = weather.data.list[i].main.temp
+		/* USING FILTER METHOD & FOREACH METHOD*/
+		const filteredData = weather.data.list.filter((perItem) =>
+			perItem.dt_txt.endsWith('06:00:00')
+		)
+
+		if (!filteredData.length) {
+			console.log('No data available at the moment. Please try again later.')
+		} else {
+			filteredData.forEach((perItem) => {
+				const date = new Date(perItem.dt_txt.split(' ')[0]).toLocaleDateString(
+					'id-ID',
+					options
+				)
+				const temperature = perItem.main.temp
 
 				console.log(`${date}: ${temperature}°C`)
-			}
+			})
 		}
+
+		/* USING FOR LOOP */
+		// for (let i = 1; i < weather.data.list.length; i++) {
+		// 	if (weather.data.list[i].dt_txt.endsWith('06:00:00')) {
+		// 		let date = new Date(
+		// 			weather.data.list[i].dt_txt.split(' ')[0]
+		// 		).toLocaleDateString('id-ID', options)
+		// 		let temperature = weather.data.list[i].main.temp
+
+		// 		console.log(`${date}: ${temperature}°C`)
+		// 	}
+		// }
 	} catch (error) {
-		console.log(error)
+		console.log(`Error! ${error.message}.`)
 	}
 }
 
